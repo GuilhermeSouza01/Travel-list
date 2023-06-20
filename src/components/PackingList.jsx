ba4abfeafda7a1cb263e0c968c740752ad321e9f
guilhermesouza01 <ghostgui60@gmail.com>
2023-06-19 22:41:37 -0300

    Create List component

--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Item from "./Item";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: true },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
 
 const PackingList = () => {
-  return <div className="list">PackingList</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item, id) => (
+          <Item item={item} key={id} />
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default PackingList;
